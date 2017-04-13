e44cc3a1681ecb69326e5c58f8e5fed5d2233fda
Evan <cato9546@gmail.com>
2017-04-13 19:25:42 -0400

    revision

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 8080
 var articles = {}
-var contents = {}
-var gcheese = {}
-var ched = {}
+var cheeses = {
+  gouda: {
+    name: 'gouda',
+    url: '/cheese.gouda'
+  },
+  feta: {
+    name: 'feta',
+    url: '/cheese.feta'
+  },
+  cheddar: {
+    name: 'cheddar',
+    url: '/cheese.cheddar'
+  }
+}
 
 function createArticle (article) {
   var id = Object.keys(articles).length
@@ -12,35 +23,30 @@ function createArticle (article) {
   articles[id] = article
 }
 
-function createChed (chedd) {
-  var id = Object.keys(ched).length
-  chedd.createdAt = new Date()
-  ched[id] = chedd
+function createCheese (cheese) {
+  var id = Object.keys(cheeses).length
+  cheeses.createdAt = new Date()
+  cheeses[id] = cheese.feta
+  cheeses[id] = cheese.gouda
+  cheeses[id] = cheese.cheddar
 }
 
-function createContent (content) {
-  var id = Object.keys(contents).length
-  content.createdAt = new Date()
-  contents[id] = content
-}
+createCheese({
+  title: 'Test',
+  content: 'more testing'
+})
 
-function createG (g) {
-  var id = Object.keys(gcheese).length
-  g.createdAt = new Date()
-  gcheese[id] = g
-}
-
-createChed({
+createCheese({
   title: 'Cheddar Cheese is tasty, very tasty. One of the better things to come out of Britain, even former colonial subjects agree.',
   content: 'Om Nom Nom Nom Nom Cheddar, unlike most foods from Britain, Cheddar is actually good.'
 })
 
-createContent({
+createCheese({
   title: 'Feta Cheese one of the finest kinds of cheese to ever exist? Controversy over question causes mass protests in Greece!',
   news: 'Rioting in Greece continues for the third consecutive day as Greeks everywhere are outraged over the rumor that Feta cheese was insulted by French Cheesemakers.'
 })
 
-createG({
+createCheese({
   title: 'Gouda Cheese described as being "good enough", Dutch satisfied with statement.',
   content: 'The Dutch are self-righteous as usual.'
 })
@@ -63,21 +69,21 @@ app.get('/', function (request, response) {
   })
 })
 
-app.get('/two', function (request, response) {
-  response.render('pages/two', {
-    contents: contents
+app.get('/cheese', function (request, response) {
+  response.render('pages/cheese', {
+    cheeses: feta
   })
 })
 
-app.get('/Gouda', function (request, response) {
-  response.render('pages/Gouda', {
-    gcheese: gcheese
+app.get('/cheese', function (request, response) {
+  response.render('pages/cheese', {
+    cheeses: gouda
   })
 })
 
-app.get('/ched', function (request, response) {
-  response.render('pages/ched', {
-    ched: ched
+app.get('/cheese', function (request, response) {
+  response.render('pages/cheese', {
+    cheeses: cheddar
   })
 })
 
